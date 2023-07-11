3249d85cad6d7b02cedc6aa07d4451c22217ce58
BabikerAlbanna <babiker@gmail.com>
2023-07-11 18:38:57 +0000

    modified:   app/index.php

--- app/index.php
+++ app/index.php
@@ -3,10 +3,6 @@
 		<title>PHP Test</title>
 	</head>
 	<body>
-<<<<<<< HEAD
-	<?php echo "Hello World, from Container! Pushed from CLI."; ?>
-=======
-	<?php echo "Hello World, from Container! Updated a second time!"; ?>
->>>>>>> 7a3a2fc2b60b0d541a5f5dd56d948ee9b6eaae2a
+	<?php echo "Hello World, from Container. Edited in Github."; ?>
 	</body>
 </html>
